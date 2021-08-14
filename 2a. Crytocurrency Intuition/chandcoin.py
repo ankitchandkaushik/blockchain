07e0219e785e055fcfb6f68c0babe56190555ffc
Ankit Chand <t-achand@expediagroup.com>
2021-08-14 13:04:28 +0530

    replacing chain by longest one using replace_chain route

--- 2a. Crytocurrency Intuition/chandcoin.py
+++ 2a. Crytocurrency Intuition/chandcoin.py
@@ -176,6 +176,18 @@ def connect_node():
                 'total_nodes' : list(blockchain.nodes)}
     return jsonify(response), 201
 
+# Replace the chain by longest chain if needed
+@app.route('/replace_chain', methods = ['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message' : 'The nodes had different chain so chain was replaced by longest one.',
+                    'new_chain' : blockchain.chain}
+    else:
+        response = {'message' : 'All good chain is the largest one.',
+                    'actual_chain' : blockchain.chain}
+    return jsonify(response), 200
+
 # Running the app
 app.run(host = '0.0.0.0', port = 5000)
 
